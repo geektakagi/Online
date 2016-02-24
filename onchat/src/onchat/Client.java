3634bc5f6a50fce9fcf1cbbedad71936eea102a6
renetbutler <Renet.pc@gmail.com>
2016-02-24 16:45:31 +0900

    fix:changed client class name

---
+++ onchat/src/onchat/Client.java
@@ -0,0 +1,119 @@
+package onchat;
+
+import java.awt.BorderLayout;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.JSplitPane;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Label;
+import javax.swing.JTextArea;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JScrollBar;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Client extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	 // Launch the application.
+	 public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Client frame = new Client();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	// Create the frame.
+	public Client() {
+		String Name = new String("Renet");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 640, 920);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		
+		textField.setBounds(17, 17, 486, 45);
+		contentPane.add(textField);
+		textField.setColumns(30);
+		
+		JButton btnSend = new JButton("Send");
+		
+
+		btnSend.setBounds(520, 32, 75, 31);
+		contentPane.add(btnSend);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(17, 81, 578, 756);
+		contentPane.add(scrollPane);
+		
+		JLabel label = new JLabel("");
+		label.setFont(new Font("Meiryo", Font.PLAIN, 25));
+		scrollPane.setViewportView(label);
+		label.setVerticalAlignment(SwingConstants.TOP);
+		
+		
+		btnSend.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(textField.getText() != ""){
+					String str = new String("<html>" + Name + " > "+ textField.getText() + "<br>");
+					str += label.getText();
+					label.setText(str);
+					textField.setText("");
+				}	
+			}
+		});
+		
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == e.VK_ENTER){
+					if(textField.getText() != ""){
+						String str = new String("<html>" + Name + " > "+ textField.getText() + "<br>");
+						str += label.getText();
+						label.setText(str);
+						textField.setText("");
+					}
+				}
+				
+			}
+		});
+		
+		
+	}
+}
